16e6dc461f42781f3bdb533d8e40383e398eea3a
quyp <1095698022@qq.com>
2022-09-30 17:56:07 +0800

    update: tokenizer

--- tokenizer.spec.ts
+++ tokenizer.spec.ts
@@ -16,3 +16,34 @@ test('tokenizer', ()=>{
       ];
     expect(tokenizer(code)).toEqual(tokens);
 })
+
+test('Paren', ()=>{
+  const code = `(`;
+  const tokens = [{ type:TokenTypes.Paren, value: "(" }]
+    expect(tokenizer(code)).toEqual(tokens);
+});
+
+
+test('add', ()=>{
+  const code = `add`;
+  const tokens = [{ type:TokenTypes.Name, value: "add" }]
+    expect(tokenizer(code)).toEqual(tokens);
+});
+
+test('number', ()=>{
+  const code = `22`;
+  const tokens = [{ type:TokenTypes.Number, value: "22" }]
+    expect(tokenizer(code)).toEqual(tokens);
+})
+
+test('(add 1 2)', ()=>{
+  const code = `(add 1 2)`;
+  const tokens = [
+    { type:TokenTypes.Paren, value: "(" },
+    { type:TokenTypes.Name, value: "add" },
+    { type:TokenTypes.Number, value: "1" },
+    { type:TokenTypes.Number, value: "2" },
+    { type:TokenTypes.Paren, value: ")" },
+  ]
+    expect(tokenizer(code)).toEqual(tokens);
+});
--- tokenizer.ts
+++ tokenizer.ts
@@ -11,11 +11,61 @@ export interface Token {
 
 export function tokenizer(code: string){
     const tokens: Token[] = [];
-    console.log(code);
+    let current = 0;
+    while(current < code.length){
+        let char = code[current];
 
+        if(char == '('){
+            tokens.push({
+                type: TokenTypes.Paren,
+                value: char
+            });
+            current++;
+            continue;
+        }
+    
+        if(char == ')'){
+            tokens.push({
+                type: TokenTypes.Paren,
+                value: char
+            });
+            current++;
+            continue;
+        }
+    
+        const LETTERS = /[a-z]/i;
+        if(LETTERS.test(char)){
+            let value = '';
+            while(LETTERS.test(char) && current< code.length){
+                value+=char;
+                char = code[++current];
+            }
+            tokens.push({
+                type: TokenTypes.Name,
+                value
+            })
+        }
+    
+        const NUMBERS = /[0-9]/;
+        if (NUMBERS.test(char)) {
+          let value = "";
+          while (NUMBERS.test(char) && current < code.length) {
+            value += char;
+            char = code[++current];
+          }
+          tokens.push({
+            type: TokenTypes.Number,
+            value,
+          });
+        }
+        // 
+        const WHITESPACE = /\s/;
+        if (WHITESPACE.test(char)) {
+          current++;
+          continue;
+        }
 
-
-
+    }
     return tokens;
 }
 
